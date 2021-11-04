a2222a5152bb51a1c78d5499a5cc214b31e70ce4
Plain Demon <89330268+Plaindemon@users.noreply.github.com>
2021-11-03 22:45:44 -0600

    work check commit

--- Develop/script.js
+++ Develop/script.js
@@ -3,3 +3,7 @@ const time = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
 // console.log(time)
 document.getElementById("currentDay").innerHTML += "Schedule: " + time + "";
 
+
+// $("#main").on("click", "div", function() {
+//     console.log("<div> was clicked");
+//   });
