6c842787e4201dedc54a73357dfc5c6302c7a05c
Plain Demon <89330268+Plaindemon@users.noreply.github.com>
2021-11-04 19:01:07 -0600

    added to js file and moved moment to new js file

--- moment.js
+++ moment.js
@@ -1,5 +1,9 @@
 const time = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+// const currentTime = moment().format("h:mm:ss");
 
 // console.log(time)
 document.getElementById("currentDay").innerHTML += "Schedule: " + time + "";
 
+// document.getElementById("hourlyTime").innerHTML += "Schedule: " + currentTime + "";
+
+// console.log(time)
--- script.js
+++ script.js
@@ -3,27 +3,40 @@
 // $(document).ready(function() {
 // $("#sortableList").click(function() {
 //     console.log("click")
-//     $("#sortableList").sortable();
-//     $("#sortableList").disableSelection();
+//     
 // })
 // })
 
-// saveBtn
+
 $(document).ready(function() {
+    // $(".displayText").add();
+    // saveBtn
     $(".saveBtn").click(function() {
         // console.log("click")
-        var taskListItem = $("textarea[name=textInputForm]").val();
-        $("p.displayText").append("<p>" + taskListItem + "</p>");
+        
         console.log("click")
     });
-});
-// trashBtn
-$(document).ready(function() {
+    // trashBtn
+
     $(".trashBtn").click(function() {
-        console.log("click")
+        // console.log("click")
         $(".displayText").remove("p");
+        console.log("clickedtrashbtn")
         
     })
-})
+    // preventDefault();
+    $('select#hourlyTimeDropdown').submit(function(e){
+        e.preventDefault();
+        var time = $('textarea#submit').val();
+        console.log(time);
+    })
+    $('select#hourlyTimeDropdown').change(function(e){
+        console.log(e.target.value);
+    })
 
-//
+
+});
+
+
+
+// 
